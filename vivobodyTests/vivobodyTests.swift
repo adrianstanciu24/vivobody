5beeb0a5974c1826d64e45fae9e08dac9a2d6abb
Adrian Stanciu <adrianstanciu24@gmail.com>
2026-03-11 16:24:44 +0200

    Initial Commit

---
+++ vivobodyTests/vivobodyTests.swift
@@ -0,0 +1,17 @@
+//
+//  vivobodyTests.swift
+//  vivobodyTests
+//
+//  Created by Adrian Stanciu on 11.03.2026.
+//
+
+import Testing
+@testable import vivobody
+
+struct vivobodyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
