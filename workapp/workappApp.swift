4355e2fd061d319d8d9d9031c973936437cf7a10
Adrian Stanciu <adrianstanciu24@gmail.com>
2026-05-18 21:06:59 +0300

    Initial Commit

---
+++ workapp/workappApp.swift
@@ -0,0 +1,32 @@
+//
+//  workappApp.swift
+//  workapp
+//
+//  Created by Adrian Stanciu on 18.05.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct workappApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
